71e33aff9c9259a8d3cc65a6f9ccb15db2588368
saitadikonda99 <saitadikonda.tech@gmail.com>
2024-04-24 21:39:22 +0530

    Basic app setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,23 @@
+
+
+import './globals.css';
+
+// import starts here 
+
+
+// import images here 
+
+
+// import icons here 
+
+
+
+export default function Home() {
+  return (
+    <div className="HomeComponent">
+        <div className="HomeComponent-in">
+            <h1>Tadikonda Sai Manikanta</h1>
+        </div>
+    </div>
+  );
+}
